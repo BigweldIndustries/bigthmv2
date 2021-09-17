14f4ef6965be2dfe729fd3074bd5ffa333a2a0ca
BigweldIndustries <75338791+BigweldIndustries@users.noreply.github.com>
2021-09-16 20:22:58 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,72 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=activity_type,name=song_name))
     print(bot.user.name)
 
+@bot.command()
+async def help(ctx, args=None):
+    help_embed = discord.Embed(colour=discord.Color.from_rgb(255, 0, 0, title="Bigthm Help")
 
+        help_embed.add_field(
+            name="Play",
+            value="Plays a song",
+            inline=False
+        )
+        help_embed.add_field(
+            name="Skip",
+            value="Skips a song",
+            inline=False
+        )
+        help_embed.add_field(
+            name="Download",
+            value="Downloads a song",
+            inline=False
+        )
+        help_embed.add_field(
+            name="Join",
+            value="Joins a vc",
+            inline=False
+        )
+        help_embed.add_field(
+            name="Leave",
+            value="Leaves vc",
+            inline=False
+        )
+        help_embed.add_field(
+            name="Pause",
+            value="Pauses a song",
+            inline=False
+        )
+        help_embed.add_field(
+            name="Queue",
+            value="Shows the queue",
+            inline=False
+        )
+        help_embed.add_field(
+            name="Repeat",
+            value="Repeats a song",
+            inline=False
+        )
+        help_embed.add_field(
+            name="Reset",
+            value="Resets the current song",
+            inline=False
+        )
+        help_embed.add_field(
+            name="Resume",
+            value="Resumes a song",
+            inline=False
+        )
+        help_embed.add_field(
+            name="Stop",
+            value="hops off",
+            inline=False
+        )
+        help_embed.add_field(
+            name="Volume",
+            value="Changes volume",
+            inline=False
+        )
+
+    await ctx.send(embed=help_embed)
 
 
 
